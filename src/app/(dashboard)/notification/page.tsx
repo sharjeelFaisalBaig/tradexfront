c0e5c60d8a1249a3dc513ba33c40c4305d12432a
JunaidAltaf2929 <junaidaltaf2929@gmail.com>
2025-11-18 13:45:22 +0500

    move notifications

---
+++ src/app/(dashboard)/notification/page.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+const NotificationsPage = () => {
+  return (
+    <>
+      <div className=" mx-auto bg-white shadow-xl rounded-xl border border-gray-200 overflow-hidden">
+        <div className="flex justify-between items-center px-4 py-3 border-b">
+          <h3 className="font-semibold text-lg">
+            All Notifications
+            <span className="ml-1 text-xs bg-gray-200 px-2 py-0.5 rounded-full">
+              4
+            </span>
+          </h3>
+        </div>
+
+        <div className="divide-y">
+          {[
+            { time: "Just now" },
+            { time: "Yesterday" },
+            { time: "2 days ago" },
+            { time: "2 days ago" },
+          ].map((item, i) => (
+            <div
+              key={i}
+              className={`px-4 py-3 ${
+                item.time === "Just now" ? "bg-gray-100" : ""
+              }`}
+            >
+              <h4 className="font-medium text-sm flex items-center gap-2">
+                <span className="text-[#00AA67] text-xl leading-none">•</span>
+                Commission received for unit #012
+                <span className="ml-auto text-xs text-gray-500">
+                  {item.time}
+                </span>
+              </h4>
+              <p className="text-sm text-gray-500 mt-1">
+                It is a long established fact that a reader will be distracted
+                by the readable content of a page.
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default NotificationsPage;
